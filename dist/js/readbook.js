47abbbaab2d050f86ed7948354f1d18eb52223e7
Jayaustin1122 <bobisjayaustin5@gmail.com>
2024-12-06 08:01:34 +0800

    Added Some

--- dist/js/readbook.js
+++ dist/js/readbook.js
@@ -63,9 +63,8 @@ async function queryBookAcrossAllUsersByTimestamp(timestampEpoch) {
                   
                         console.log("Book details found:", bookDetails);
                         document.getElementById('book-title').textContent = title;
-                        document.getElementById('uthor-name').textContent = `By ${author}`;
-                        document.getElementById('book-author').textContent = `By ${author}`;
-                        document.getElementById('book-publication-date').textContent = `Publication Date: ${publicationDate}`;
+                        document.getElementById('author-name').textContent = `By ${author}`;
+                        document.getElementById('book-author').textContent = `By ${author} | Publication Date: ${publicationDate}`;
                         document.getElementById('book-genre').textContent = `Genre: ${genre}`;
                         document.getElementById('book-content').textContent = content;
 
